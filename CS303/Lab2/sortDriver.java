e800c96f983fd364c72b66439faa45f345e73d5e
Vaishak Menon <vaishakkmenon@gmail.com>
2021-08-31 08:22:07 -0500

    all files working, project complete

--- CS303/Lab2/sortDriver.java
+++ CS303/Lab2/sortDriver.java
@@ -12,14 +12,33 @@ public class sortDriver
 
         int[] input_100 = new int[]{};
         String[] sArray = new String[]{};
-        int[] sorted_100 = new int[]{};
 
         int[] input_1000 = new int[]{};
         String[] sArray2 = new String[]{};
-        int[] sorted_1000 = new int[]{};
+
+        int[] input_5000 = new int[]{};
+        String[] sArray3 = new String[]{};
+        
+        int[] input_10000 = new int[]{};
+        String[] sArray4 = new String[]{};
+
+        int[] input_50000 = new int[]{};
+        String[] sArray5 = new String[]{};
+
+        int[] input_100000 = new int[]{};
+        String[] sArray6 = new String[]{};
+
+        int[] input_500000 = new int[]{};
+        String[] sArray7 = new String[]{};
 
         File f1 = new File("C:\\Users\\vaish\\SophYearCS\\CS303\\Lab2\\input_100.txt");
         File f2 = new File("C:\\Users\\vaish\\SophYearCS\\CS303\\Lab2\\input_1000.txt");
+        File f3 = new File("C:\\Users\\vaish\\SophYearCS\\CS303\\Lab2\\input_5000.txt");
+        File f4 = new File("C:\\Users\\vaish\\SophYearCS\\CS303\\Lab2\\input_10000.txt");
+        File f5 = new File("C:\\Users\\vaish\\SophYearCS\\CS303\\Lab2\\input_50000.txt");
+        File f6 = new File("C:\\Users\\vaish\\SophYearCS\\CS303\\Lab2\\input_100000.txt");
+        File f7 = new File("C:\\Users\\vaish\\SophYearCS\\CS303\\Lab2\\input_500000.txt");
+
 
         try 
         {
@@ -45,16 +64,106 @@ public class sortDriver
             e.printStackTrace();
         }
 
+        try 
+        {
+            Scanner sc = new Scanner(f3);
+            sArray3 = sc.nextLine().trim().split(" ");
+            input_5000 = new int[sArray3.length];
+            sc.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            e.printStackTrace();
+        }
+
+        try 
+        {
+            Scanner sc = new Scanner(f4);
+            sArray4 = sc.nextLine().trim().split(" ");
+            input_10000 = new int[sArray4.length];
+            sc.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            e.printStackTrace();
+        }
+
+        try 
+        {
+            Scanner sc = new Scanner(f5);
+            sArray5 = sc.nextLine().trim().split(" ");
+            input_50000 = new int[sArray5.length];
+            sc.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            e.printStackTrace();
+        }
+
+        try 
+        {
+            Scanner sc = new Scanner(f6);
+            sArray6 = sc.nextLine().trim().split(" ");
+            input_100000 = new int[sArray6.length];
+            sc.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            e.printStackTrace();
+        }
+
+        try 
+        {
+            Scanner sc = new Scanner(f7);
+            sArray7 = sc.nextLine().trim().split(" ");
+            input_500000 = new int[sArray7.length];
+            sc.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            e.printStackTrace();
+        }
+
         long sTime_100 = System.nanoTime();
-        sorted_100 = iS.iSort(input_100);
+        input_100 = iS.iSort(input_100);
         long eTime_100 = System.nanoTime() - sTime_100;
 
         System.out.println("Time taken to insertion sort array of 100 numbers: " + eTime_100);
 
         long sTime_1000 = System.nanoTime();
-        sorted_1000 = iS.iSort(input_1000);
+        input_1000 = iS.iSort(input_1000);
         long eTime_1000 = System.nanoTime() - sTime_1000;
 
         System.out.println("Time taken to insertion sort array of 1000 numbers: " + eTime_1000);
+
+        long sTime_5000 = System.nanoTime();
+        input_5000 = iS.iSort(input_5000);
+        long eTime_5000 = System.nanoTime() - sTime_5000;
+
+        System.out.println("Time taken to insertion sort array of 5000 numbers: " + eTime_5000);
+
+        long sTime_10000 = System.nanoTime();
+        input_10000 = iS.iSort(input_10000);
+        long eTime_10000 = System.nanoTime() - sTime_10000;
+
+        System.out.println("Time taken to insertion sort array of 10000 numbers: " + eTime_10000);
+
+        long sTime_50000 = System.nanoTime();
+        input_50000 = iS.iSort(input_50000);
+        long eTime_50000 = System.nanoTime() - sTime_50000;
+
+        System.out.println("Time taken to insertion sort array of 50000 numbers: " + eTime_50000);
+
+        long sTime_100000 = System.nanoTime();
+        input_100000 = iS.iSort(input_100000);
+        long eTime_100000 = System.nanoTime() - sTime_100000;
+
+        System.out.println("Time taken to insertion sort array of 100000 numbers: " + eTime_100000);
+
+        long sTime_500000 = System.nanoTime();
+        input_500000 = iS.iSort(input_500000);
+        long eTime_500000 = System.nanoTime() - sTime_500000;
+
+        System.out.println("Time taken to insertion sort array of 500000 numbers: " + eTime_500000);
     }    
 }
