f2e13968bd1a8ba4674222d2be6d5a5305afbd1d
Vaishak Menon <vaishakkmenon@gmail.com>
2021-08-31 07:31:59 -0500

    fully working insertion sort

--- CS303/Lab2/insertionSort.java
+++ CS303/Lab2/insertionSort.java
@@ -10,7 +10,7 @@ public class insertionSort
         {
             key = array[j];
             i = j - 1;
-            while(i > 0 && array[i] > key)
+            while(i >= 0 && array[i] > key)
             {
                 array[i + 1] = array[i];
                 i = i - 1;
