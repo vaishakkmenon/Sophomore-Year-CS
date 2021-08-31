70911b8740b35f20fbc200c2eff8b355335dc1dd
Vaishak Menon <vaishakkmenon@gmail.com>
2021-08-30 19:46:49 -0500

    Lab 2 insertion sort code written, not tested

---
+++ CS303/Lab2/insertionSort.java
@@ -0,0 +1,23 @@
+package CS303.Lab2;
+
+public class insertionSort
+{
+    public int[] iSort(int[] array)
+    {
+        int i = 0;
+        int key = 0;
+        for (int j = 1; j < array.length; j++)
+        {
+            key = array[j];
+            i = array[j-1];
+            while(i > 0 && array[i] > key)
+            {
+                array[i + 1] = array[i];
+                array[i] = array[i - 1];
+            }
+            array[i + 1] = key;
+        }
+
+        return array;
+    }
+}
