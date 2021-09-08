1f970acb81166fb2e6a4f672bf344827d2c6287a
Vaishak Menon <vaishakkmenon@gmail.com>
2021-09-08 09:34:16 -0500

    possibly working heapsort

--- CS303/Lab4/HeapSort2.java
+++ CS303/Lab4/HeapSort2.java
@@ -2,9 +2,32 @@ package CS303.Lab4;
 
 public class HeapSort2 
 {
+    public void sort(int[] A)
+    {
+        int n = A.length;
+        buildMaxHeap(A);
+        for (int i = n; i > 0; i--) 
+        {
+            int temp = A[0];
+            A[0] = A[i];
+            A[i] = temp;
+            n -= 1;
+            maxHeapify(A, n, i);
+        }
+    }
+
+    public void buildMaxHeap(int[] A)
+    {
+        int n = A.length;
+        for(int i = A.length/2; i >= 1; i--)
+        {
+            maxHeapify(A, n, i);
+        }
+    }
+
     public void maxHeapify(int[] A, int n, int i)
     {
-        int largest;
+        int largest = i;
         int l = LEFT(i);
         int r = RIGHT(i);
         
@@ -13,12 +36,14 @@ public class HeapSort2
         else
             largest = i;
 
-        if(r <= n &&  A[r] > A[i])
+        if(r <= n &&  A[r] > A[largest])
             largest = r;
         if(largest != i)
         {
+            int temp = A[i];
             A[i] = A[largest];
-            maxHeapify(A,i,largest);
+            A[largest] = temp;
+            maxHeapify(A,n,largest);
         }
 
     }    
