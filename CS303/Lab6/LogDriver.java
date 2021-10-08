8d7ea2475d6b3c93de1e5de023436ee6c39aece0
Vaishak Menon <vaishakkmenon@gmail.com>
2021-10-08 13:15:16 -0500

    works with states with multiple words, extra credit

--- CS303/Lab6/LogDriver.java
+++ CS303/Lab6/LogDriver.java
@@ -3,18 +3,13 @@ package CS303.Lab6;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-public class LogDriver 
-{
-    public static void main(String[] args)
+public class LogDriver {
+    public static void main(String[] args) 
     {
 
         LogSort ls = new LogSort();
-
-        Map<Integer, String> original = new HashMap<>();
 
         File f = new File("NovelSortInput.txt");
         ArrayList<String> cities = new ArrayList<String>();
@@ -23,25 +18,54 @@ public class LogDriver
         try 
         {
             Scanner sc = new Scanner(f);
-            while (sc.hasNextLine())
+            while (sc.hasNextLine()) 
             {
                 String[] log = sc.nextLine().trim().split(" ");
-                String city = log[0];
-                String time = "";
-                if (!(log[0].isEmpty())) 
+                //Index for last element
+                int last = log.length-1;
+
+                if (log.length == 2) 
                 {
-                    time = log[1];
-                    String[] timeSplit = time.split(":");
-                    String s = "";
-                    for (int i = 0; i < timeSplit.length; i++) 
+                    String city = log[0];
+                    String time = "";
+                    if (!(log[0].isEmpty())) 
                     {
-                        s += timeSplit[i];
+                        time = log[last];
+                        String[] timeSplit = time.split(":");
+                        String s = "";
+                        for (int i = 0; i < timeSplit.length; i++) 
+                        {
+                            s += timeSplit[i];
+                        }
+                        int newTime = Integer.parseInt(s);
+                        cities.add(city);
+                        times.add(newTime);
                     }
-                    int newTime = Integer.parseInt(s);
-                    cities.add(city);
-                    times.add(newTime);
-                    original.put(newTime,city);
                 }
+                else
+                {
+                    String city = "";
+                    for(int i = 0; i < last;i++)
+                    {
+                        city = city + log[i];
+                    }
+
+                    String time = "";
+                    if (!(log[0].isEmpty())) 
+                    {
+                        time = log[last];
+                        String[] timeSplit = time.split(":");
+                        String s = "";
+                        for (int i = 0; i < timeSplit.length; i++) 
+                        {
+                            s += timeSplit[i];
+                        }
+                        int newTime = Integer.parseInt(s);
+                        cities.add(city);
+                        times.add(newTime);
+                    }
+                }
+
             }
             sc.close();
         } 
@@ -51,31 +75,19 @@ public class LogDriver
         }
 
         ArrayList<String> newTimes = ls.convert(times);
-        
-        for(String s : cities)
-        {
-            System.out.print(s + " ");
-        }
 
+        System.out.println("Unsorted Cities: ");
+        for (String s : cities) {System.out.print(s + " ");}
         System.out.println();
-        for(String x : newTimes)
-        {
-            System.out.print(x + " ");
-        }
+        for (String x : newTimes) {System.out.print(x + " ");}
 
         ls.iSort(cities, times);
         newTimes = ls.convert(times);
 
         System.out.println();
-
-        for(String s : cities)
-        {
-            System.out.print(s + " ");
-        }
+        System.out.println("Sorted Cities: ");
+        for (String s : cities) {System.out.print(s + " ");}
         System.out.println();
-        for(String x : newTimes)
-        {
-            System.out.print(x + " ");
-        }
+        for (String x : newTimes) {System.out.print(x + " ");}
     }
 }
